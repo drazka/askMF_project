675a380c9dceb4a67ebb87ef993ddd47997cdb28
Izabela Draga <izabdr@gmail.com>
2018-04-16 14:10:25 +0200

    przepisywanie do class

--- askMF2.py
+++ askMF2.py
@@ -1,12 +1,8 @@
-nip_extractor = NIPExtractor()
-
-nip_extractor.check_nip('nip') #return slownik: nr nip, komunikat, printscreen
-nip_extractor.check_list_of_nips(['nips']) #retern: tupla(nr nip, komunikat, printcreen)
-
+from splinter import Browser
 
 class NIPExtractor():
     def __init__(self):
-        self._browser = Browser('chrome', headless=True)
+        self._browser = Browser('chrome')
         #scraped_data = []
         url = 'https://ppuslugi.mf.gov.pl/'
         self._browser.driver.set_window_size(940, 580)
@@ -22,3 +18,9 @@ class NIPExtractor():
     def check_list_of_nips(self, nips):
         pass
 
+
+
+nip_extractor = NIPExtractor()
+
+nip_extractor.check_nip('nip') #return slownik: nr nip, komunikat, printscreen
+nip_extractor.check_list_of_nips(['nips']) #retern: tupla(nr nip, komunikat, printcreen)
